ece05f9e5acb3045b0a7deb347473d3738bdd6e8
Colin Dean <git@cad.cx>
2019-02-02 13:01:08 -0500

    Adds a quick debug print of retrieval URL

--- src/retrieve.rs
+++ src/retrieve.rs
@@ -1,7 +1,11 @@
 const BLOCKSTACK_PROFILE_ENDPOINT_TEMPLATE: &str = "https://core.blockstack.org/v1/users/:username";
-
+use std::env;
 pub fn retrieve_user_profile(username: &str) -> Result<serde_json::Value, reqwest::Error> {
     let url = build_profile_url(&username);
+
+    if env::var("DEBUG").is_ok() {
+        eprintln!("DEBUG: Retrieving {}", url);
+    }
 
     reqwest::get(url.as_str())?
         .error_for_status()?
