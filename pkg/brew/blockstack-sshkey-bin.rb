6937b475abc3784958c75fb93afb56036d8969e7
Colin Dean <git@cad.cx>
2019-02-02 13:53:29 -0500

    Updates Homebrew formula for 0.1.4

--- pkg/brew/blockstack-sshkey-bin.rb
+++ pkg/brew/blockstack-sshkey-bin.rb
@@ -1,14 +1,14 @@
 class BlockstackSshkeyBin < Formula
-    version '0.1.2'
+    version '0.1.4'
     desc "Retrieve the SSH key for Blockstack users given their Blockstack ID"
     homepage "https://github.com/colindean/blockstack-sshkey"
 
     if OS.mac?
       url "https://github.com/colindean/blockstack-sshkey/releases/download/#{version}/blockstack-sshkey-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "5f1326468516e56916ecc4da66bd022a4b1192f161292786386b731e9d9204ff"
+      sha256 "25e2d08c1aa17c4f6a275586430a8d0c8257e34448346cd6467237878b25ec38"
     elsif OS.linux?
       url "https://github.com/colindean/blockstack-sshkey/releases/download/#{version}/blockstack-sshkey-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b751a8270cda5f4ae1b42b830f293d2ca0338d4e7a9cfaba372c91c3f03f1a85"
+      sha256 "4364271fa3be7af1af1c80a1f4b303171970e0543874c5bb5a6936925514a466"
     end
 
     def install
